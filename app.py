bd8d0f3bba664f8eb8d26f0cac77b54eae1e6753
dpmilclean01-ai <dpmilclean01@gmail.com>
2026-02-05 11:22:05 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -78,10 +78,23 @@ if 'logado' not in st.session_state:
 @st.cache_resource
 def conectar_gsheets():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    if not os.path.exists("credenciais.json"): st.error("🚨 'credenciais.json' não encontrado!"); st.stop()
-    creds = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json", scope)
-    client = gspread.authorize(creds)
-    return client.open("SistemaDP_DB")
+    
+    # TENTATIVA 1: Conexão via Segredos do Streamlit Cloud (NUVEM)
+    if "gcp_service_account" in st.secrets:
+        creds_dict = st.secrets["gcp_service_account"]
+        creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+        client = gspread.authorize(creds)
+        return client.open("SistemaDP_DB")
+
+    # TENTATIVA 2: Conexão via Arquivo Local (SEU PC)
+    elif os.path.exists("credenciais.json"):
+        creds = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json", scope)
+        client = gspread.authorize(creds)
+        return client.open("SistemaDP_DB")
+        
+    else:
+        st.error("🚨 ERRO: Credenciais não encontradas (Nem segredos, nem arquivo JSON).")
+        st.stop()
 
 def verificar_login(user, pwd):
     if user == "adm" and pwd == "123": return True
